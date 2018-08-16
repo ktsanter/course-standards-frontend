a2e6e51de94a564bf6382623c76842676c017a62
SpunkyAardvark123 <ktsanter@gmail.com>
2018-08-15 23:22:17 -0400

    enough for today

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const app = function () {
 	const API_KEY = 'MVstandardsAPI';
 	const DEPARTMENTS = ['cte', 'english', 'math', 'socialstudies', 'science', 'worldlanguage'];
 	const NO_COURSE = 'NO_COURSE';
+	const NO_SELECTION = 'NO_SELECTION';
 	const AP_KEY = 'AP';
 	const AP_POLICY_DOC = 'https://drive.google.com/open?id=1CnvIf-ZaTD5INn8ACzZi942oRpEE887EuJFipJ5eFNI';
 
@@ -71,6 +72,27 @@ const app = function () {
 			})
 	}
 
+	function _getStandardsSelections (keyname) {
+		_setNotice('loading selections...');
+
+		fetch(_buildApiUrl('selections', null, keyname))
+			.then((response) => response.json())
+			.then((json) => {
+				//console.log('json.status=' + json.status);
+				if (json.status !== 'success') {
+					_setNotice(json.message);
+				}
+				//console.log('json.data: ' + JSON.stringify(json.data));
+				
+				_renderStandardsSelection(keyname, json.data);
+				_setNotice('');
+			})
+			.catch((error) => {
+				_setNotice('Unexpected error loading course standards');
+				console.log(error);
+			})
+	}
+
 	function _postCourseStandards (coursename) {
 		_setNotice('posting course standards...');
 		var postData = _packageCourseStandardsForPost(coursename);
@@ -82,7 +104,7 @@ const app = function () {
 			})
 			.then((response) => response.json())
 			.then((json) => {
-				console.log('json.status=' + json.status);
+				//console.log('json.status=' + json.status);
 				if (json.status !== 'success') {
 					_setNotice(json.message);
 				}
@@ -96,11 +118,12 @@ const app = function () {
 			})
 	}
 	
-	function _buildApiUrl (datasetname, coursename) {
+	function _buildApiUrl (datasetname, coursename, keyname) {
 		let url = API_BASE;
 		url += '?key=' + API_KEY;
-		url += datasetname !== null ? '&dataset=' + datasetname : '';
-		url += coursename !== null ? '&coursename=' + coursename : '';
+		url += datasetname && datasetname !== null ? '&dataset=' + datasetname : '';
+		url += coursename && coursename !== null ? '&coursename=' + coursename : '';
+		url += keyname && keyname !== null ? '&keyname=' + keyname : '';
 		//console.log('buildApiUrl: url=' + url);
 		return url;
 	}
@@ -181,45 +204,16 @@ const app = function () {
 			var elemStandard;
 
 			if (keyInfo.keyType == 'text') {
-				elemStandard = document.createElement('div');
-				elemStandard.innerHTML = '  ' + keyInfo.keyPrompt + ' ' + standardValue;
+				elemStandard = _createStandardsElement_text(keyName, keyInfo, standardValue);		
 				
 			} else if (keyInfo.keyType == 'tf') {
-				elemStandard = document.createElement('div');
-				var elemPrompt = document.createElement('span');
-				elemPrompt.innerHTML = keyInfo.keyPrompt;
-				var elemCheckbox = document.createElement('input');
-				elemCheckbox.type = 'checkbox';
-				elemCheckbox.checked = standardValue;
-				
-				elemStandard.appendChild(elemPrompt);
-				elemStandard.appendChild(elemCheckbox);
-
-				if (keyInfo.keyName == AP_KEY  && standardValue) {
-					var elemLink = document.createElement('a');
-					elemLink.text = apPolicyDoc.text;
-					elemLink.href = apPolicyDoc.link;
-					elemLink.target = "_blank";
-					elemStandard.appendChild(elemLink);
-				}				
+				elemStandard = _createStandardsElement_tf(keyName, keyInfo, standardValue);
 				
 			} else if (keyInfo.keyType == 'link') {
-				var linkInfo = JSON.parse(standardValue);
-				console.log(keyInfo.keyName + ': ' + JSON.stringify(linkInfo) + ', ' + linkInfo.text);
-				if (linkInfo.text) {
-					elemStandard = document.createElement('div');
-					var elemLink = document.createElement('a');
-					elemLink.text = linkInfo.text;
-					elemLink.href = linkInfo.link;
-					elemLink.target = "_blank";
-					elemStandard.appendChild(elemLink);
-				} else {
-					elemStandard = document.createElement('span');
-				}
+				elemStandard = _createStandardsElement_link(keyName, keyInfo, standardValue);
 				
 			} else {
-				elemStandard = document.createElement('div');
-				elemStandard.innerHTML = keyInfo.keyName + ' unrecognized type: ' + keyInfo.keyType;
+				elemStandard = _createStandardsElement_unrecognized(keyName, keyInfo, standardValue);
 			}
 			
 			elemWrapper.appendChild(elemStandard);
@@ -228,11 +222,107 @@ const app = function () {
 		return elemWrapper;
 	}
 	
-	function _packageCourseStandardsForPost (coursename) {
-		//console.log('_packageCourseStandardsForPost: ' + coursename);
-		//console.log('courseList: ' + JSON.stringify(ssData.courseList));
-		//console.log('standardsData: ' + JSON.stringify(ssData.standardsData));
+	function _createStandardsElement_text(keyName, keyInfo, standardValue) {
+		elemStandard = document.createElement('div');
 		
+		var elemPrompt = document.createElement('span');
+		elemPrompt.innerHTML = keyInfo.keyPrompt;
+		
+		var elemValue = document.createElement('input');
+		elemValue.type = 'text';
+		elemValue.id = keyName;
+		elemValue.value = standardValue;
+		elemValue.maxLength = 200;
+		elemValue.size = 40;
+		
+		elemStandard.appendChild(elemPrompt);
+		elemStandard.appendChild(elemValue);
+
+		if (keyInfo.keyIncludeSelect) {
+			var elemButton = document.createElement('button');
+			elemButton.id = 'btn' + keyName;
+			elemButton.innerHTML = '&#11167;';
+			elemButton.addEventListener('click', function() {
+					handleDownButtonClick(this);
+				}, false);
+		
+			elemStandard.appendChild(elemButton);
+		}
+
+		return elemStandard;
+	}
+
+				
+	function _createStandardsElement_tf(keyName, keyInfo, standardValue) {
+		elemStandard = document.createElement('div');
+		var elemPrompt = document.createElement('span');
+		elemPrompt.innerHTML = keyInfo.keyPrompt;
+		var elemCheckbox = document.createElement('input');
+		elemCheckbox.id = keyName;
+		elemCheckbox.type = 'checkbox';
+		elemCheckbox.checked = standardValue;
+				
+		elemStandard.appendChild(elemPrompt);
+		elemStandard.appendChild(elemCheckbox);
+		
+		if (keyInfo.keyName == AP_KEY  && standardValue) {
+			var elemLink = document.createElement('a');
+			elemLink.text = apPolicyDoc.text;
+			elemLink.href = apPolicyDoc.link;
+			elemLink.target = "_blank";
+			elemStandard.appendChild(elemLink);
+		}				
+		return elemStandard;
+	}
+
+				
+	function _createStandardsElement_link(keyName, keyInfo, standardValue) {
+		var linkInfo = JSON.parse(standardValue);
+		//console.log(keyInfo.keyName + ': ' + JSON.stringify(linkInfo) + ', ' + linkInfo.text);
+		if (linkInfo.text) {
+			elemStandard = document.createElement('div');
+			var elemLink = document.createElement('a');
+			elemLink.text = linkInfo.text;
+			elemLink.href = linkInfo.link;
+			elemLink.target = "_blank";
+			elemStandard.appendChild(elemLink);
+		} else {
+			elemStandard = document.createElement('span');
+		}
+		return elemStandard;
+	}
+	
+	function _createStandardsElement_unrecognized(keyName, keyInfo, standardValue) {
+		elemStandard = document.createElement('div');
+		elemStandard.innerHTML = keyInfo.keyPrompt + '(' + keyName + ' unrecognized type: ' + keyInfo.keyType + ')';
+		return elemStandard;
+	}
+	
+	function _renderStandardsSelection(keyname, data) {
+		var selections = data.selections;	
+		var currentValue = document.getElementById(keyname).value;
+		
+		var elemWrapper = document.createElement('div');
+
+		var elemSelect = document.createElement('select');
+		elemSelect.id = 'select' + keyname;
+		
+		for (var i = 0; i < selections.length; i++) {
+			var elemOption = document.createElement('option');
+			elemOption.text = selections[i];
+			elemOption.value = 'optSelect' + i;
+			elemOption.id = elemOption.value;
+			elemOption.selected = (currentValue == selections[i]);
+			elemSelect.append(elemOption);
+		}
+		elemSelect.addEventListener('change', function() {selectOptionChanged(this, keyname)}, false);
+		
+		elemWrapper.appendChild(elemSelect);
+		elemWrapper.appendChild(document.createElement('br'));
+		document.getElementById(keyname).parentNode.appendChild(elemWrapper);
+	}
+	
+	function _packageCourseStandardsForPost (coursename) {		
 		var standards = ssData.standardsData.standards;
 		var fullKeyList = ssData.standardsData.categoryInfo.fullKeyList;
 		var postData = {
@@ -270,9 +360,22 @@ const app = function () {
 		console.log('test edit');
 		var coursename = document.getElementById('selectCourse').value;
 		if (coursename != NO_COURSE) {
-			// change some standards values and then...
-			//_postCourseStandards(coursename);
+			var standards = ssData.standardsData.standards;
+			standards['DB_requiredreplies'] = 5;
+			_postCourseStandards(coursename);
+			//_getCourseStandards(coursename);
 		}
+	}
+	
+	function handleDownButtonClick(btn) {
+		//console.log('down button clicked: ' + btn.id.slice(3));
+		_getStandardsSelections(btn.id.slice(3));
+	}
+	
+	function selectOptionChanged(elem, keyname) {
+		var newStandardsValue = document.getElementById(elem.value).text;
+		console.log('select option changed for ' + keyname + ': ' + newStandardsValue);
+		elem.parentNode.parentNode.removeChild(elem.parentNode.parentNode.lastChild);
 	}
 	
 	return {
